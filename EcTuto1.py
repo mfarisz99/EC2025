385434df02794854e21b1974bd6e4700aaf63e5e
Mfarisz <93848554+mfarisz99@users.noreply.github.com>
2024-10-27 19:18:50 +0800

    Update EcTuto1.py

--- EcTuto1.py
+++ EcTuto1.py
@@ -8,7 +8,7 @@ import random
 #POP_SIZE: Number of Chromosomes in our list.
 POP_SIZE = 500
 
-MUT_RATE = st.slider("Select the mutation rate", min_value=0.0, max_value=1.0, value=0.10, step=0.01)
+MUT_RATE = st.slider("Select the mutation rate", min_value=0.0, max_value=0.5, value=0.10, step=0.01)
 #MUT_RATE: Rate at which our string will be changed.
 ##MUT_RATE = st.text_input("Enter the mutation rate")
 ##MUT_RATE = 0.10
